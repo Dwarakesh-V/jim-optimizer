661ce9567c4e07018b002a10e4cace4180dbc104
SUDHARSAN SARAVANAN <sudharsansaravanan2623@gmail.com>
2026-02-22 17:33:25 +0530

    update: Main.java - Print each stage, pom.xml - silence the errors

--- src/main/java/optimizer/Main.java
+++ src/main/java/optimizer/Main.java
@@ -13,24 +13,31 @@ public class Main {
 
         G.reset();
 
+        System.out.println("-> Starting Jimple Optimizer");
+        System.out.println("-> Input directory: " + inputDir);
+
         Options.v().set_prepend_classpath(true);
         Options.v().set_process_dir(Collections.singletonList(inputDir));
         Options.v().set_output_format(Options.output_format_jimple);
         Options.v().set_allow_phantom_refs(true);
         Options.v().set_whole_program(true);
 
-        // IMPORTANT
         Options.v().set_main_class("Test");
 
+        System.out.println("-> Loading classes...");
         Scene.v().loadNecessaryClasses();
 
+        System.out.println("-> Registering optimization pipeline...");
         PackManager.v().getPack("jtp")
                 .add(new Transform("jtp.pipeline",
                         new OptimizerPipeline()));
 
+        System.out.println("-> Running optimizer passes...");
         PackManager.v().runPacks();
+
+        System.out.println("-> Writing optimized Jimple...");
         PackManager.v().writeOutput();
 
-        System.out.println("Done.");
+        System.out.println("-> Optimization complete. Done.");
     }
 }
